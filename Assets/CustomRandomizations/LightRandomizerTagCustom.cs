ee49c01de8ed6ab3dc11cdd8a508de5ade3dc83a
Sameer25-py <sameergb25@outlook.com>
2025-06-16 17:11:01 +0500

    feat: create custom light randomizer

---
+++ Assets/CustomRandomizations/LightRandomizerTagCustom.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Perception.Randomization.Parameters;
+using UnityEngine.Perception.Randomization.Randomizers;
+using UnityEngine.Perception.Randomization.Samplers;
+
+namespace CustomRandomizations
+{
+    // Add this Component to any GameObject that you would like to be randomized. This class must have an identical name to
+// the .cs file it is defined in.
+    [RequireComponent(typeof(Light))]
+    public class LightRandomizerTagCustom : RandomizerTag {}
+
+    [Serializable]
+    [AddRandomizerMenu("Light Randomizer")]
+    public class LightRandomizer : Randomizer
+    {
+        // Sample FloatParameter that can generate random floats in the [0,360) range. The range can be modified using the
+        // Inspector UI of the Randomizer.
+        public FloatParameter LightIntensity;
+
+        public ColorRgbParameter RGBColor;
+
+        protected override void OnIterationStart()
+        {
+            var tags = tagManager.Query<LightRandomizerTagCustom>();
+            foreach (var tag in tags)
+                if(tag.TryGetComponent(out Light light))
+                {
+                    light.intensity = LightIntensity.Sample();
+                    light.color = RGBColor.Sample();
+                }
+        }
+    }
+}
